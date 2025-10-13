830a42dd852423dd913a681be9a4a5376fa1b20c
Charbelakplogan2002 <acharbel02@gmail.com>
2025-10-13 08:44:58 +0000

    Ajout de exo2.c

---
+++ exo2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Exo2
+
+int add (int nb1, int nb2) {
+    return nb1 + nb2;
+}
+
+int main() {
+    int result = add(6, 4);
+    printf("Result: %d\n", result);
+    return 0;
+}
